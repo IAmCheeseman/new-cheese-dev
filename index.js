9ed1cef784700a4479743053ae3c2dc1ab584a04
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-01-31 16:42:55 -0500

    Added page titles

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
+
 $(function() {
     $("#navbar-parent").load("/navbar.html");
     $("#footer-parent").load("/footer.html");
 });
+
+
+let title = document.getElementById('title').innerHTML
+document.title = `Cheeseman Dev - ${title}`
