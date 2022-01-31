521875162d21c7e8e66502b65d6313de6f746460
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-01-31 11:57:00 -0500

    Loading Devlogs from json

---
+++ tabs/devlogs/devlogs.js
@@ -0,0 +1,22 @@
+
+
+function loadDevlogs()
+{
+    // Fetching the devlog button
+    fetch('./devlogButton.html')
+    .then(request => request.text())
+    .then(devlogButton => {
+        // Fetching the devlogs json
+        fetch('./devlogs.json')
+        .then(request => request.json())
+        .then(data => {
+            // Creating the devlogs
+            data.devlogs.forEach(element => {
+                let devButtonStr = devlogButton.replace(">name", `>${element.name}`).replace(">desc", `>${element.description}`)
+                document.getElementById('content').insertAdjacentHTML('beforeend', devButtonStr)
+            })
+        })
+    })
+}
+
+loadDevlogs();
