97bb49380db4439550a3468873524f3669a8e5d4
Xander <cheeseman085@gmail.com>
2022-01-31 19:57:17 -0500

    Embed Test

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ $(function() {
 });
 
 
+function createEmbed(desc, img)
+{
+    let html = `<meta property="og:title" content="title-here" /><meta property="og:description" content="desc-here" /><meta name="theme-color" content="#303434" /><meta name="author" content="Cheeseman" /><meta property="og:image" content="img-here"></meta>`
+    html.replace('title-here', document.title)
+    html.replace('desc-here', desc)
+    html.replace('img-here', img)
+    let head = document.head
+    head.insertAdjacentHTML('beforeend', html)
+}
+
+
 let title = document.getElementById('title').innerHTML
 document.title = `Cheeseman Dev - ${title}`
