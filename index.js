f0c4265b0eb42c8dd9df6ce8238156e3a52a2fa3
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2022-02-01 08:19:06 -0500

    Added favicon

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ $(function() {
     $("#footer-parent").load("/footer.html");
 });
 
+function setFavicon(img){
+    let head = document.querySelector('head');
+    let favicon = document.createElement('link');
+    favicon.rel = 'shortcut icon'
+    favicon.href = img
+
+    head.appendChild(favicon);
+}
+
+setFavicon('/favicon.png')
+
 let title = document.getElementById('title').innerHTML
 document.title = `Cheeseman Dev - ${title}`
